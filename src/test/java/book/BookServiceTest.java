a9b41b553db7cdcbbcfcfa78f1a7c64e584d0965
GarryProhor <igorprohorchenko@gmail.com>
2022-11-04 15:56:15 +0300

    tests

--- src/test/java/book/BookServiceTest.java
+++ src/test/java/book/BookServiceTest.java
@@ -1,13 +1,67 @@
 package book;
 
+import book.model.Book;
+import book.service.BookService;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class BookServiceTest {
+    @Mock
+    BookService bookService;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
 
     @Test
-    void contextLoads() {
+    public void getAllBooksTest() {
+        int date = new Date().getYear();
+        List<Book> list = new ArrayList<>();
+        list.add(new Book(1L, "Title 1", "Author 1", date));
+        list.add(new Book(2L, "Title 2", "Author 2", date));
+
+        when(bookService.findAllBooks()).thenReturn(list);
+
+        List<Book> books = bookService.findAllBooks();
+
+        assertEquals(2, books.size());
+        verify(bookService, times(1)).findAllBooks();
+    }
+
+    @Test
+    public void getBookByIdTest() {
+
+        int date = new Date().getYear();
+        Book book = new Book(1L, "Title 1", "Author 1", date);
+
+        when(bookService.findOne(1L)).thenReturn(book);
+
+        Book book1 = bookService.findOne(1L);
+
+        verify(bookService, times(1)).findOne(1L);
+    }
+
+    @Test
+    public void createBookTest() {
+
+        int date = new Date().getYear();
+        Book book = new Book(1L, "Title 1", "Author 1", date);
+        Book bookCreated = bookService.save(book);
+
+        verify(bookService, times(1)).save(book);
     }
 
 }
